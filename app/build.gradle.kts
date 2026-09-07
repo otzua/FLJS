606b5b5dd7a7f02cf8ecf9ab3e76904cfd4b4102
otzua <sglaxy936@gmail.com>
2026-09-08 02:47:59 +0530

    chore(release): bump version to 2.2.0 (versionCode 24)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.pikamander2.japanesequizz"
         minSdk = 24
         targetSdk = 36
-        versionCode = 23
-        versionName = "2.3"
+        versionCode = 24
+        versionName = "2.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
